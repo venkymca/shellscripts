0bc820a1e024e5530bcd23d7b6eadf4a66cbfa49
venkymca <venkychepuri132@gmail.com>
2025-04-05 12:47:05 +0530

    Create echoex.sh

---
+++ echoex.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Example of using echo with different options
+
+echo -n "This is on the same line "
+echo "and this is on the next line."
+
+printf "Line 1\nLine 2\nLine 3"
+
+printf "This will not interpret escape sequences like\nor\t"
